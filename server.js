4368eb19577ca35beaaa284bfcf2764a6679e530
VladF146 <vlad.fast146@gmail.com>
2022-10-25 10:49:10 +0200

    configured another port for users without a dedicated .env file

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express = require("express");
 
 const server = express();
 
+const port = process.env.PORT || 5000;
+
 server.get("/", (req, res) => {
   res.json({ message: "Hello from the server." });
 });
 
-server.listen(process.env.PORT, () => {
-  console.log(`Server listening on port ${process.env.PORT}`);
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
