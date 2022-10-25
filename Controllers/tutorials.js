a483063868fb12549225532c51a8541727999310
VladF146 <vlad.fast146@gmail.com>
2022-10-25 15:50:18 +0200

    established the connection to the database inside the controller

--- Controllers/tutorials.js
+++ Controllers/tutorials.js
@@ -1,33 +1,60 @@
-const getAllTutorials = (req, res) => {
-  res.json({ message: `Path: ${req.path}, Method: ${req.method}` });
+const Tutorial = require("../Models/tutorials");
+
+const getAllTutorials = async (req, res) => {
+  try {
+    const allTutorials = await Tutorial.find({});
+    res.status(200).json(allTutorials);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
-const getSingleTutorial = (req, res) => {
-  res.json({
-    message: `Path: ${req.path}, Method: ${req.method}, Id: ${req.params.id}`,
-  });
+const getSingleTutorial = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const singleTutorial = await Tutorial.findById(id);
+
+    res.status(200).json(singleTutorial);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
-const createSingleTutorial = (req, res) => {
-  res.json({
-    message: `Path: ${req.path}, Method: ${req.method}, Body:  ${JSON.stringify(
-      req.body
-    )}`,
-  });
+const createSingleTutorial = async (req, res) => {
+  const { title, content } = req.body;
+
+  try {
+    const singleTutorial = await Tutorial.create({ title, content });
+    res.status(200).json(singleTutorial);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
-const updateSingleTutorial = (req, res) => {
-  res.json({
-    message: `Path: ${req.path}, Method: ${req.method}, Id: ${
-      req.params.id
-    }, Body: ${JSON.stringify(req.body)}`,
-  });
+const updateSingleTutorial = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const singleTutorial = await Tutorial.findOneAndUpdate(
+      { _id: id },
+      { ...req.body }
+    );
+    res.status(200).json(singleTutorial);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
-const deleteSingleTutorial = (req, res) => {
-  res.json({
-    message: `Path: ${req.path}, Method: ${req.method}, Id: ${req.params.id}`,
-  });
+const deleteSingleTutorial = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const singleTutorial = await Tutorial.findOneAndDelete({ _id: id });
+    res.status(200).json(singleTutorial);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = {
