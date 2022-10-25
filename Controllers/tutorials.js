949af96171024b9e55e2f6a34b1af4c73669a56b
VladF146 <vlad.fast146@gmail.com>
2022-10-25 13:18:26 +0200

    added more response information for better testing

--- Controllers/tutorials.js
+++ Controllers/tutorials.js
@@ -3,19 +3,31 @@ const getAllTutorials = (req, res) => {
 };
 
 const getSingleTutorial = (req, res) => {
-  res.json({ message: `Path: ${req.path}, Method: ${req.method}` });
+  res.json({
+    message: `Path: ${req.path}, Method: ${req.method}, Id: ${req.params.id}`,
+  });
 };
 
 const createSingleTutorial = (req, res) => {
-  res.json({ message: `Path: ${req.path}, Method: ${req.method}` });
+  res.json({
+    message: `Path: ${req.path}, Method: ${req.method}, Body:  ${JSON.stringify(
+      req.body
+    )}`,
+  });
 };
 
 const updateSingleTutorial = (req, res) => {
-  res.json({ message: `Path: ${req.path}, Method: ${req.method}` });
+  res.json({
+    message: `Path: ${req.path}, Method: ${req.method}, Id: ${
+      req.params.id
+    }, Body: ${JSON.stringify(req.body)}`,
+  });
 };
 
 const deleteSingleTutorial = (req, res) => {
-  res.json({ message: `Path: ${req.path}, Method: ${req.method}` });
+  res.json({
+    message: `Path: ${req.path}, Method: ${req.method}, Id: ${req.params.id}`,
+  });
 };
 
 module.exports = {
