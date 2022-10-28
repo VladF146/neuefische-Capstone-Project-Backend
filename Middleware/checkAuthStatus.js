3408139d680b85d947d8b230fa54e60c2834c513
VladF146 <vlad.fast146@gmail.com>
2022-10-28 14:05:49 +0200

    send back 401 Unauthorized, if token can't be verified

--- Middleware/checkAuthStatus.js
+++ Middleware/checkAuthStatus.js
@@ -1,25 +1,29 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-import jwt from 'jsonwebtoken';
-import User from '../Models/users.js';
+import jwt from "jsonwebtoken";
+import User from "../Models/users.js";
 
 dotenv.config();
 
 const checkAuthStatus = async (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res
       .status(401)
-      .json({ error: 'You must be logged in to make this request!' });
+      .json({ error: "You must be logged in to make this request!" });
   }
 
   try {
     const { id } = jwt.verify(
       token,
-      process.env.MY_JWT_SECRET
-        || 'S2T7iqfnSIL1RWP9N8BCCs5jEgDwYRJ0ZbzNA6XF43dO',
+      process.env.MY_JWT_SECRET ||
+        "S2T7iqfnSIL1RWP9N8BCCs5jEgDwYRJ0ZbzNA6XF43dO"
     );
+
+    if (!id) {
+      return res.status(401).json({ error: "Token could not be verified!" });
+    }
 
     const user = await User.findOne({ _id: id });
     req.userId = user.id;
