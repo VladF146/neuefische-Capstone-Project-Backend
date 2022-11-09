55ce89037c767eb16b50268b198a9dd7dabc93a7
VladF146 <vlad.fast146@gmail.com>
2022-11-09 14:55:18 +0100

    configured the server to accept bigger payloads

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const mongoDbUri =
   process.env.MONGODB_URI ||
   "mongodb+srv://VladF146:jU7%259CEZ%24iFn3YSs9e2z@courseapp.9lwqmqm.mongodb.net/?retryWrites=true&w=majority";
 
-server.use(express.json());
+app.use(express.json({ limit: "10mb", extended: true }));
+app.use(
+  express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 })
+);
 server.use(cors());
 
 server.use("/api/users", userRouter);
