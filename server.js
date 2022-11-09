14bc500b32d02f1c2b51f8e28757a61b46719067
VladF146 <vlad.fast146@gmail.com>
2022-11-09 15:14:17 +0100

    renamed app to server

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const mongoDbUri =
   process.env.MONGODB_URI ||
   "mongodb+srv://VladF146:jU7%259CEZ%24iFn3YSs9e2z@courseapp.9lwqmqm.mongodb.net/?retryWrites=true&w=majority";
 
-app.use(express.json());
+server.use(express.json({ limit: "10mb", extended: true }));
+server.use(
+  express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 })
+);
 server.use(cors());
 
 server.use("/api/users", userRouter);
